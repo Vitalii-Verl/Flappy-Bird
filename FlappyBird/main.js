e2a4a9de3fbd4d7e2ac3b5b32a7f63445afcac85
Vitalii-Verl <66761213+Vitalii-Verl@users.noreply.github.com>
2025-12-28 21:00:11 +0200

    Delete FlappyBird directory

--- FlappyBird/main.js
+++
@@ -1,5 +0,0 @@
-import { Game } from "./game.js"
-
-const canvas = document.querySelector('canvas')
-const game = new Game(canvas)
-game.loadAssets().then(() => game.start())
--- FlappyBird/utils.js
+++
@@ -1,7 +0,0 @@
-export function loadImage(img, src) {
-  return new Promise((resolve, reject) => {
-    img.onload = resolve
-    img.onerror = reject
-    img.src = src
-  })
-}
